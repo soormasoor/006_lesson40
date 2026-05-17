4ad7de58d1110f2856ac1179bf4fa2b490d9ad15
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-05-17 13:11:44 +0400

    Switched to v2.0.0; Divided time render logic, divided Timer start logic, fixed flash alert

--- pomodoroTask/index.js
+++ pomodoroTask/index.js
@@ -1,135 +1,157 @@
-const DURATIONS = {
-  work: 0.05 * 60 * 1000, // TEMPORARILY CHANGED MINUTES FOR DEBUGGING
-  shortBreak: 0.05 * 60 * 1000,
-  longBreak: 0.05 * 60 * 1000,
-};
+// const DURATIONS = {
+//   work: 0.1 * 60 * 1000, // TEMPORARILY CHANGED MINUTES FOR DEBUGGING
+//   shortBreak: 0.1 * 60 * 1000,
+//   longBreak: 0.1 * 60 * 1000,
+// };
 
-const AUTO_SWITCH_DELAY = 3000;
+// const AUTO_SWITCH_DELAY = 3000;
 
-const phaseEl = document.getElementById("phase");
-const timerEl = document.getElementById("timer");
-const sessionsEl = document.getElementById("sessions");
-const messageEl = document.getElementById("message");
+// const phaseEl = document.getElementById("phase");
+// const timerEl = document.getElementById("timer");
+// const sessionsEl = document.getElementById("sessions");
+// const messageEl = document.getElementById("message");
 
-const startBtn = document.getElementById("startBtn");
-const pauseBtn = document.getElementById("pauseBtn");
-const resumeBtn = document.getElementById("resumeBtn");
-const resetBtn = document.getElementById("resetBtn");
+// const startBtn = document.getElementById("startBtn");
+// const pauseBtn = document.getElementById("pauseBtn");
+// const resumeBtn = document.getElementById("resumeBtn");
+// const resetBtn = document.getElementById("resetBtn");
 
-let completedSessions = 0;
+// let completedSessions = 0;
 
-let timer = {
-  end: null,
-  intervalId: null,
-};
+// let timer = {
+//   end: null,
+//   intervalId: null,
+//   flashIntervalId: null,
+//   isFlashing: false,
+// };
 
-let isRunning = false;
-let isPaused = false;
+// let isRunning = false;
+// let isPaused = false;
 
-function calculateTime() {
-  const diff = timer.end - Date.now();
+// function calculateTime() {
+//   const diff = timer.end - Date.now();
 
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
+//   const minutes = Math.floor((diff / (1000 * 60)) % 60);
+//   const seconds = Math.floor((diff / 1000) % 60);
 
-  const pad = (i) => String(i).padStart(2, "0");
+//   const pad = (i) => String(i).padStart(2, "0");
 
-  timerEl.textContent = `${pad(minutes)}:${pad(seconds)}`;
+//   timerEl.textContent = `${pad(minutes)}:${pad(seconds)}`;
 
-  return diff;
-}
+//   return diff;
+// }
 
-function clearTime() {
-  isRunning = false;
-  isPaused = false;
-  timerEl.textContent = "00:00";
-  timer = {
-    end: null,
-    intervalId: null,
-  };
-}
+// function clearTime() {
+//   isRunning = false;
+//   isPaused = false;
+//   timerEl.textContent = "00:00";
+//   timer = {
+//     end: null,
+//     intervalId: null,
+//     flashIntervalId: null,
+//     isFlashing: false,
+//   };
+// }
 
-function startPhase(phase) {
-  if (!DURATIONS.hasOwnProperty(phase)) {
-    throw new TypeError("Invalid phase name");
-  }
-  if (isRunning) {
-    return null;
-  }
+// function startPhase(phase) {
+//   if (!DURATIONS.hasOwnProperty(phase)) {
+//     throw new TypeError("Invalid phase name");
+//   }
+//   if (isRunning) {
+//     return null;
+//   }
 
-  isRunning = true;
+//   isRunning = true;
 
-  const timeLeft = DURATIONS[phase];
-  timer.end = new Date();
-  timer.end.setTime(timer.end.getTime() + timeLeft);
+//   const timeLeft = DURATIONS[phase];
+//   timer.end = new Date();
+//   timer.end.setTime(timer.end.getTime() + timeLeft);
+//   calculateTime();
 
-  switch (phase) {
-    case "work":
-      phaseEl.textContent = "Work";
-      break;
-    case "shortBreak":
-      phaseEl.textContent = "Short Break";
-      break;
-    case "longBreak":
-      phaseEl.textContent = "Long Break";
-  }
+//   switch (phase) {
+//     case "work":
+//       phaseEl.textContent = "Work";
+//       break;
+//     case "shortBreak":
+//       phaseEl.textContent = "Short Break";
+//       break;
+//     case "longBreak":
+//       phaseEl.textContent = "Long Break";
+//   }
 
-  timer.intervalId = setInterval(() => {
-    if (calculateTime() <= 1000) {
-      clearInterval(timer.intervalId);
-      clearTime();
+//   timer.intervalId = setInterval(() => {
+//     if (calculateTime() <= 10000) {
+//       timer.flashIntervalId = setInterval(() => {
+//         timerEl.style.color = timer.isFlashing ? "red" : "white";
+//         timer.isFlashing = !timer.isFlashing;
+//       }, 1000);
+//     }
 
-      if (phase === "work") {
-        sessionsEl.textContent = `Completed sessions: ${++completedSessions}`;
+//     if (calculateTime() <= 1000) {
+//       clearInterval(timer.intervalId);
+//       clearInterval(timer.flashIntervalId);
+//       clearTime();
 
-        setTimeout(() => {
-          startPhase(completedSessions % 4 === 0 ? "longBreak" : "shortBreak");
-        }, AUTO_SWITCH_DELAY);
-      } else {
-        setTimeout(() => {
-          startPhase("work");
-        }, AUTO_SWITCH_DELAY);
-      }
-    }
-  }, 1000);
-}
+//       if (phase === "work") {
+//         sessionsEl.textContent = `Completed sessions: ${++completedSessions}`;
 
-function pauseTimer() {
-  if (isPaused || !isRunning) {
-    return null;
-  }
+//         setTimeout(() => {
+//           startPhase(completedSessions % 4 === 0 ? "longBreak" : "shortBreak");
+//         }, AUTO_SWITCH_DELAY);
+//       } else {
+//         setTimeout(() => {
+//           startPhase("work");
+//         }, AUTO_SWITCH_DELAY);
+//       }
+//     }
+//   }, 800);
+// }
 
-  clearInterval(timer.intervalId);
-  isPaused = true;
-}
+// function pauseTimer() {
+//   if (isPaused || !isRunning) {
+//     return null;
+//   }
 
-function resumeTimer() {
-  if (!isPaused) {
-    return null;
-  }
+//   clearInterval(timer.intervalId);
+//   isPaused = true;
+// }
 
-  timer.intervalId = setInterval(() => {
-    if (calculateTime() <= 1000) {
-      clearInterval(timer.intervalId);
-      clearTime();
+// function resumeTimer() {
+//   if (!isPaused) {
+//     return null;
+//   }
 
-      if (phase === "work") {
-        sessionsEl.textContent = `Completed sessions: ${++completedSessions}`;
-      }
-    }
-  }, 1000);
+//   calculateTime();
+//   timer.intervalId = setInterval(() => {
+//     if (calculateTime() <= 1000) {
+//       clearInterval(timer.intervalId);
+//       clearTime();
 
-  isPaused = false;
-}
+//       if (phaseEl.textContent === "Work") {
+//         sessionsEl.textContent = `Completed sessions: ${++completedSessions}`;
 
-startBtn.addEventListener("click", () => {
-  if (!isRunning && !isPaused) {
-    startPhase("work");
-  }
-});
+//         setTimeout(() => {
+//           startPhase(completedSessions % 4 === 0 ? "longBreak" : "shortBreak");
+//         }, AUTO_SWITCH_DELAY);
+//       } else {
+//         setTimeout(() => {
+//           startPhase("work");
+//         }, AUTO_SWITCH_DELAY);
+//       }
+//     }
+//   }, 1000);
 
-pauseBtn.addEventListener("click", pauseTimer);
-resumeBtn.addEventListener("click", resumeTimer);
-resetBtn.addEventListener("click", resetTimer);
+//   isPaused = false;
+// }
 
-render();
+// startBtn.addEventListener("click", () => {
+//   if (!isRunning && !isPaused) {
+//     startPhase("work");
+//   }
+// });
+
+// pauseBtn.addEventListener("click", pauseTimer);
+// resumeBtn.addEventListener("click", resumeTimer);
+// resetBtn.addEventListener("click", resetTimer);
+
+// render();
---
+++ pomodoroTask/v2.js
@@ -0,0 +1,214 @@
+"use strict";
+const DURATIONS = {
+  work: 0.1 * 60 * 1000, // debug durations
+  shortBreak: 0.1 * 60 * 1000,
+  longBreak: 0.1 * 60 * 1000,
+};
+
+const AUTO_SWITCH_DELAY = 3000;
+
+const phaseEl = document.getElementById("phase");
+const timerEl = document.getElementById("timer");
+const sessionsEl = document.getElementById("sessions");
+const messageEl = document.getElementById("message");
+
+const startBtn = document.getElementById("startBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const resumeBtn = document.getElementById("resumeBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let completedSessions = 0;
+
+const timer = {
+  phase: null,
+  end: null,
+  timeLeft: null,
+
+  intervalId: null,
+  flashIntervalId: null,
+
+  isFlashing: false,
+};
+
+let isRunning = false;
+let isPaused = false;
+
+function remainingTime() {
+  return timer.end - Date.now();
+}
+
+function formatTime(millis) {
+  const minutes = Math.floor((millis / (1000 * 60)) % 60);
+  const seconds = Math.floor((millis / 1000) % 60);
+
+  const pad = (i) => String(i).padStart(2, "0");
+
+  return `${pad(minutes)}:${pad(seconds)}`;
+}
+
+function renderTime() {
+  timerEl.textContent = formatTime(Math.max(remainingTime(), 0));
+}
+
+function updatePhase(phase) {
+  switch (phase) {
+    case "work":
+      phaseEl.textContent = "Work";
+      break;
+
+    case "shortBreak":
+      phaseEl.textContent = "Short Break";
+      break;
+
+    case "longBreak":
+      phaseEl.textContent = "Long Break";
+      break;
+  }
+}
+
+function startFlashing() {
+  if (timer.flashIntervalId) {
+    return;
+  }
+
+  timer.flashIntervalId = setInterval(() => {
+    timerEl.style.color = timer.isFlashing ? "red" : "white";
+
+    timer.isFlashing = !timer.isFlashing;
+  }, 500);
+}
+
+function stopFlashing() {
+  clearInterval(timer.flashIntervalId);
+
+  timer.flashIntervalId = null;
+  timer.isFlashing = false;
+
+  timerEl.style.color = "white";
+}
+
+function clearTimer() {
+  clearInterval(timer.intervalId);
+  clearInterval(timer.flashIntervalId);
+
+  timer.intervalId = null;
+  timer.flashIntervalId = null;
+
+  stopFlashing();
+
+  isRunning = false;
+  isPaused = false;
+}
+
+function finishPhase() {
+  const finishedPhase = timer.phase;
+
+  clearTimer();
+
+  if (finishedPhase === "work") {
+    sessionsEl.textContent = `Completed sessions: ${++completedSessions}`;
+
+    const nextPhase = completedSessions % 4 === 0 ? "longBreak" : "shortBreak";
+
+    setTimeout(() => {
+      startPhase(nextPhase);
+    }, AUTO_SWITCH_DELAY);
+  } else {
+    setTimeout(() => {
+      startPhase("work");
+    }, AUTO_SWITCH_DELAY);
+  }
+}
+
+function runTimerLoop() {
+  timer.intervalId = setInterval(() => {
+    renderTime();
+
+    const timeLeft = remainingTime();
+
+    if (timeLeft <= 10000) {
+      startFlashing();
+    }
+
+    if (timeLeft <= 0) {
+      finishPhase();
+    }
+  }, 250);
+}
+
+function startPhase(phase) {
+  if (!DURATIONS.hasOwnProperty(phase)) {
+    throw new TypeError("Invalid phase");
+  }
+
+  if (isRunning) {
+    return;
+  }
+
+  clearTimer();
+
+  timer.phase = phase;
+  timer.end = Date.now() + DURATIONS[phase];
+
+  isRunning = true;
+
+  updatePhase(phase);
+  renderTime();
+
+  runTimerLoop();
+}
+
+function pauseTimer() {
+  if (!isRunning || isPaused) {
+    return;
+  }
+
+  clearInterval(timer.intervalId);
+
+  timer.timeLeft = remainingTime();
+
+  isPaused = true;
+  isRunning = false;
+}
+
+function resumeTimer() {
+  if (!isPaused) {
+    return;
+  }
+
+  timer.end = Date.now() + timer.timeLeft;
+
+  isPaused = false;
+  isRunning = true;
+
+  renderTime();
+
+  runTimerLoop();
+}
+
+function resetTimer() {
+  clearTimer();
+
+  completedSessions = 0;
+
+  sessionsEl.textContent = "Completed sessions: 0";
+
+  phaseEl.textContent = "Press START";
+  timerEl.textContent = "00:00";
+
+  timer.phase = null;
+  timer.end = null;
+  timer.timeLeft = null;
+}
+
+startBtn.addEventListener("click", () => {
+  if (!isRunning && !isPaused) {
+    startPhase("work");
+  }
+});
+
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+resetTimer();
